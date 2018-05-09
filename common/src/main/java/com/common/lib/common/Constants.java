5d7b87edc5503064654370db7a16ddb084972369
刘勇 <yong.liu@fnconn.com>
2018-05-09 16:54:49 +0800

    add constant

---
+++ common/src/main/java/com/common/lib/common/Constants.java
@@ -0,0 +1,44 @@
+package com.common.lib.common;
+
+/**
+ * @author
+ * @time 2016/10/17
+ * @des 常量配置类
+ */
+
+public class Constants {
+
+    public static final int PageSize = 20;
+
+    /**
+     * 人民币符号
+     **/
+    public static String RMB_UNIT = "¥";
+    /**
+     * 美元符号
+     **/
+    public static String US_UNIT = "$";
+
+    public static final String LoginToken = "LOGIN_TOKEN";
+    public static final String LoginAccount = "LOGIN_ACCOUNT";
+    public static final String LoginUserName = "LOGIN_USERNAME";
+    public static final String LoginUName = "LOGIN_UNAME";
+    public static final String LoginUserId = "LOGIN_USERID";
+    public static final String LoginUserMobile = "LOGIN_Mobile";
+    public static final String UserInfoJson = "USER_INFO_JSON";
+    public static final String latitude = "LATITUDE";
+    public static final String lontitude = "LONTITUDE";
+    public static final String myLocationAddr = "MY_LOCATIONADDR";
+    public static final String cityName = "CITY_NAME";
+    public static final String FIRSTTIME_SHOW_TIPS = "FIRSTTIME_SHOW_TIPS";
+
+    /**
+     * 上红点上一次最后查询时间
+     */
+    public static final String LAST_QUERY_TIME = "LAST_QUERY_TIME";
+    public static final String lastTimeCCCount = "lastTimeCCCount";
+    public static final String lastTimeSQCount = "lastTimeSQCount";
+    public static final String IsArraveBeforeSix = "IsArraveBeforeSix";
+    public static final String IsPayInvoice = "IsPayInvoice";
+
+}
